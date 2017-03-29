decec05edd511c91ff226761226755217a2867dc
applehat <cameronchunn@gmail.com>
2017-03-29 13:17:52 -0500

    good

--- app.js
+++ app.js
@@ -42,5 +42,5 @@ app.get('/', function (req, res) {
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-	console.log('Example app listening on port 80!')
+	console.log('Example app listening on port '+port);
 })
