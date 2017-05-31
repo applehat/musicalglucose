b0110abe0b285f592c4489ed244f3d0412cba2a2
Cameron Chunn <applehat@users.noreply.github.com>
2017-05-31 13:18:17 -0500

    Update musical.js

--- public/musical.js
+++ public/musical.js
@@ -53,7 +53,18 @@
 	}
 
 	test(0);
+	
+	var data = {
+		  // A labels array that can contain any sort of values
+		  labels: 'mg/dL',
+		  // Our series array that contains series objects or in this case series data arrays
+		  series: notes
+		};
 
-
+		// Create a new line chart object where as first parameter we pass in a selector
+		// that is resolving to our chart container element. The Second parameter
+		// is the actual data object.
+		new Chartist.Line('.ct-chart', data);
+	
 
 })();
